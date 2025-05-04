96e2d3e178dd5fc93f3e074b99c0cc32267b7594
Kyrylo Sheludchenko <kirill.shelydchenko@gmail.com>
2025-05-04 12:56:07 +0200

    practice

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ function getData() {
 }
 
 function onSuccess(result) {
-  console.log("Success", result);
+  console.log("Data:", result);
 }
 
 function onFailure(err) {
-  console.log("Err", err);
+  console.log("Err:", err);
 }
 
 async function start() {
   try {
     const result = await getData();
-    onSuccess(result)
+    onSuccess(result);
   } catch (err) {
-    onFailure(err)
+    onFailure(err);
   }
 }
 
@@ -36,8 +36,160 @@ async function start2() {
     .catch((err) => onFailure(err));
 
   // cathces the error but the rest of the code still runs
-  onSuccess(result)
+  onSuccess(result);
 }
 
-start();
-start2();
+// start();
+// start2();
+
+// 1)
+
+async function getGreeting() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Hello, World!");
+    }, 1000);
+  });
+}
+
+async function mainFunction() {
+  const result = await getGreeting();
+  console.log(result);
+}
+
+// mainFunction();
+
+// 2)
+
+async function simulateNetworkRequest(num) {
+  const random = Math.floor(Math.random() * 2);
+
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (random === 1) {
+        resolve(`Fetched Data ${num}`);
+      } else {
+        reject("Error");
+      }
+    }, 1000);
+  });
+}
+
+async function getDataNetwork() {
+  try {
+    const data = await simulateNetworkRequest();
+    onSuccess(data);
+  } catch (error) {
+    onFailure(error);
+  }
+}
+
+// getDataNetwork()
+
+// 3)
+
+async function fetchUserData() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Fetched User Data");
+    }, 1000);
+  });
+}
+
+async function fetchPostData() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Posted Data");
+    }, 1300);
+  });
+}
+
+async function paralleExecution() {
+  try {
+    const result = await Promise.all([fetchUserData(), fetchPostData()]);
+    onSuccess(result);
+  } catch (error) {
+    onFailure(error);
+  }
+}
+
+// paralleExecution()
+
+// 4)
+
+async function sequentialExecution() {
+  try {
+    const userData = await fetchUserData();
+    const userPosts = await fetchPostData();
+
+    onSuccess({ userData, userPosts });
+  } catch (error) {
+    onFailure(error);
+  }
+}
+
+// sequentialExecution()
+
+// 5)
+
+async function simulateDelay(min, max) {
+  const time = Math.round(Math.random() * (max - min) + min) * 1000;
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve(`Resolved in ${time}ms`);
+    }, time);
+  });
+}
+
+async function delayInvocation() {
+  try {
+    const result = await simulateDelay(1, 3);
+    onSuccess(result);
+  } catch (error) {
+    onFailure(error);
+  }
+}
+
+// delayInvocation()
+
+// 6)
+
+async function timeoutReject() {
+  const start = new Date();
+
+  const data = await simulateDelay(1, 4);
+
+  const end = new Date();
+  const timeInSeconds = (end - start) / 1000;
+
+  if (timeInSeconds > 3) {
+    throw new Error("Timeout");
+  }
+
+  return data;
+}
+
+// timeoutReject().then(onSuccess).catch(onFailure);
+
+// 7)
+
+const promises = [
+  simulateNetworkRequest(1),
+  simulateNetworkRequest(2),
+  simulateNetworkRequest(3),
+];
+
+async function promiseAny(arr) {
+  try {
+    const result = await Promise.any(arr)
+
+    onSuccess(result)
+  } catch (error) {
+    onFailure(error)
+  }
+}
+
+promiseAny(promises)
+
+// 8)
+
